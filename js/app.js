32fca3ed5cd0f67413e5c101c23f37e826fb3fe2
Bernard Historillo <bernardhistorillo1@gmail.com>
2024-12-02 18:42:06 +0800

    Update home page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
+
 var app = new Framework7({
-    // App root element
     el: '#app',
     // App Name
     name: 'My App',
@@ -7,14 +7,49 @@ var app = new Framework7({
     panel: {
         swipe: true,
     },
-    // Add default routes
     routes: [
         {
-            path: '/about/',
-            url: 'about.html',
+            name: 'home',
+            path: '/',
+            url: './pages/home.html',
+        },
+        {
+            name: 'settings',
+            path: '/settings',
+            url: './pages/settings.html',
+        },
+        {
+            name: 'news',
+            path: '/news/',
+            url: './pages/news.html',
+            options: {
+                animate: false,
+            },
+        },
+        {
+            name: 'users',
+            path: '/users/',
+            componentUrl: './pages/users.html',
+            options: {
+                props: {
+                    users: ['John Doe', 'Vladimir Kharlampidi', 'Timo Ernst'],
+                },
+            },
+            on: {
+                pageAfterIn: function test (e, page) {
+                    // do something after page gets into the view
+                },
+                pageInit: function (e, page) {
+                    // do something when page initialized
+                },
+            }
+        },
+        // Default route, match to all pages (e.g. 404 page)
+        {
+            path: '(.*)',
+            url: './pages/404.html',
         },
     ],
-    // ... other parameters
 });
 
 var mainView = app.views.create('.view-main');
