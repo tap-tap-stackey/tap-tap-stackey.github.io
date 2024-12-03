017be7e6cb7299e09a6039e1cbfbf5c2e0b05a9c
Bernard Historillo <bernardhistorillo1@gmail.com>
2024-12-03 09:27:47 +0800

    Update routes

--- js/app.js
+++ js/app.js
@@ -14,37 +14,37 @@ var app = new Framework7({
             path: '/',
             url: 'https://bernardhistorillo.github.io/tap-tap-stackey/pages/home.html',
         },
-        {
-            name: 'settings',
-            path: '/settings',
-            url: './' + baseUrl + 'pages/settings.html',
-        },
-        {
-            name: 'news',
-            path: '/news/',
-            url: './pages/news.html',
-            options: {
-                animate: false,
-            },
-        },
-        {
-            name: 'users',
-            path: '/users/',
-            componentUrl: './pages/users.html',
-            options: {
-                props: {
-                    users: ['John Doe', 'Vladimir Kharlampidi', 'Timo Ernst'],
-                },
-            },
-            on: {
-                pageAfterIn: function test (e, page) {
-                    // do something after page gets into the view
-                },
-                pageInit: function (e, page) {
-                    // do something when page initialized
-                },
-            }
-        },
+        // {
+        //     name: 'settings',
+        //     path: '/settings',
+        //     url: './' + baseUrl + 'pages/settings.html',
+        // },
+        // {
+        //     name: 'news',
+        //     path: '/news/',
+        //     url: './pages/news.html',
+        //     options: {
+        //         animate: false,
+        //     },
+        // },
+        // {
+        //     name: 'users',
+        //     path: '/users/',
+        //     componentUrl: './pages/users.html',
+        //     options: {
+        //         props: {
+        //             users: ['John Doe', 'Vladimir Kharlampidi', 'Timo Ernst'],
+        //         },
+        //     },
+        //     on: {
+        //         pageAfterIn: function test (e, page) {
+        //             // do something after page gets into the view
+        //         },
+        //         pageInit: function (e, page) {
+        //             // do something when page initialized
+        //         },
+        //     }
+        // },
         // Default route, match to all pages (e.g. 404 page)
         {
             path: '(.*)',
