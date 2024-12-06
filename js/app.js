2345645624047637c08e48d7509c218e7cbd79f2
Bernard Historillo <bernardhistorillo1@gmail.com>
2024-12-06 09:58:12 +0800

    Update routes

--- js/app.js
+++ js/app.js
@@ -33,18 +33,16 @@ $(document).ready(function() {
                 name: 'select-language',
                 path: directory + '/select-language',
                 url: baseUrl + 'pages/select-language.html',
-            },
-            {
-                path: '(.*)',
-                url: './pages/404.html',
-            },
+            }
         ],
     });
 
     app.views.create('.view-main');
 
+    // Initialize Sheets
     initializeIntroductionSheet();
     initializeDeleteAccountSheet();
+    initializeDailyRewardsSheet();
 });
 
 // Home
@@ -135,4 +133,53 @@ $(document).on("click", "#delete-account", function () {
             deleteAccountSheet.close();
         });
     }, 2000);
+});
+
+// Daily Rewards
+let dailyRewardsSheet;
+let initializeDailyRewardsSheet = function() {
+    let content = '    <div class="sheet-modal background-image-cover" style="background-image:url(\'img/bg-3.webp\')">';
+    content += '           <div class="sheet-modal-inner">';
+    content += '               <div class="page-content display-flex flex-direction-column justify-content-space-between" style="padding:14px 12px">';
+    content += '                   <div class="display-flex justify-content-end">';
+    content += '                       <div class="display-flex justify-content-center align-items-center rounded-8 hide-daily-rewards-sheet" style="padding:6px; border:1px solid #ffffff; line-height:0">';
+    content += '                           <i class="f7-icons text-color-white display-block" style="font-size:1.3em; font-weight:800">xmark</i>';
+    content += '                       </div>';
+    content += '                   </div>';
+    content += '                   <div class="display-flex flex-direction-column align-items-center">';
+    content += '                       <div class="text-color-white text-align-center" style="font-size:1.4em; margin-bottom:30px; line-height:1.1em">Daily Rewards</div>';
+    content += '                       <div class="display-flex" style="margin:-2px">';
+    content += '                            <div class="" style="padding:2px; width:33.33333%">';
+    content += '                                <div class="card bg-color-7 rounded-8 no-margin" style="border:1px solid white; height:55.8px">';
+    content += '                                    <div class="card-content card-content-padding rounded-8" style="padding:6px">';
+    content += '                                        <div class="text-color-white" style="font-size:0.8em; margin-bottom:8px">Amateur&nbsp;<span class="text-color-3">1/11</span></div>';
+    content += '                                            <div style="padding:0 4px">';
+    content += '                                                <div class="bg-color-1" style="margin-bottom:4px; border-radius:7px; width:calc(100% - 4px); padding:2px">';
+    content += '                                                    <div class="bg-color-6" style="height:10px; width:60%; border-radius:5px"></div>';
+    content += '                                                </div>';
+    content += '                                            </div>';
+    content += '                                        </div>';
+    content += '                                    </div>';
+    content += '                                </div>';
+    content += '                                <div class="text-align-center" style="margin-bottom:12px">';
+    content += '                                    <button type="button" class="button button-fill bg-color-5 rounded-8 text-color-white" style="font-size:1.2em; padding:22px 0; width:210px">Claim</button>';
+    content += '                                </div>';
+    content += '                            </div>';
+    content += '                        </div>';
+    content += '                    </div>';
+    content += '               </div>';
+    content += '           </div>';
+    content += '       </div>';
+
+    dailyRewardsSheet = app.sheet.create({
+        content: content
+    });
+};
+
+$(document).on("click", "#show-daily-rewards-sheet", function () {
+    dailyRewardsSheet.open()
+});
+
+$(document).on("click", ".hide-daily-rewards-sheet", function () {
+    dailyRewardsSheet.close()
 });
