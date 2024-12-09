ab73adcb8604d9a55f23132a0e6276b53eeda9e2
Bernard Historillo <bernardhistorillo1@gmail.com>
2024-12-09 10:20:50 +0800

    Add Troop page

--- js/app.js
+++ js/app.js
@@ -2,8 +2,6 @@
 
 let fullUrl = window.location.href;
 let environment = fullUrl.includes("https://") ? "production" : "local";
-let directory = (environment === "production") ? '/tap-tap-stackey' : '';
-let baseUrl = (environment === "production") ? 'https://bernardhistorillo.github.io/tap-tap-stackey/' : './';
 
 let app;
 let routes = [
@@ -11,6 +9,11 @@ let routes = [
         name: 'home',
         path: '/',
         url: './pages/home.html',
+    },
+    {
+        name: 'troop',
+        path: '/',
+        url: './pages/troop.html',
     },
     {
         name: 'settings',
@@ -149,7 +152,7 @@ $(document).on("click", "#delete-account", function () {
 // Daily Rewards
 let dailyRewardsSheet;
 let initializeDailyRewardsSheet = function() {
-    let content = '    <div class="sheet-modal background-image-cover" style="background-image:url(\'img/bg-3.webp\')">';
+    let content = '    <div class="sheet-modal background-image-cover" id="daily-rewards-sheet" style="background-image:url(\'img/bg-3.webp\'); height:482px">';
     content += '           <div class="sheet-modal-inner">';
     content += '               <div class="page-content display-flex flex-direction-column justify-content-space-between" style="padding:14px 12px">';
     content += '                   <div class="display-flex justify-content-end">';
@@ -158,24 +161,99 @@ let initializeDailyRewardsSheet = function() {
     content += '                       </div>';
     content += '                   </div>';
     content += '                   <div class="display-flex flex-direction-column align-items-center">';
-    content += '                       <div class="text-color-white text-align-center" style="font-size:1.4em; margin-bottom:30px; line-height:1.1em">Daily Rewards</div>';
-    content += '                       <div class="display-flex" style="margin:-2px">';
-    content += '                            <div class="" style="padding:2px; width:33.33333%">';
-    content += '                                <div class="card bg-color-7 rounded-8 no-margin" style="border:1px solid white; height:55.8px">';
+    content += '                       <div class="text-color-white text-align-center" style="font-size:1.6em; margin-bottom:30px; line-height:1.1em">Daily Rewards</div>';
+    content += '                       <div class="display-flex" style="width:100%; padding:0 30px">';
+    content += '                            <div class="" style="padding:5px; width:25%">';
+    content += '                                <div class="card rounded-8 no-margin daily-reward-day active" style="height:80px">';
     content += '                                    <div class="card-content card-content-padding rounded-8" style="padding:6px">';
-    content += '                                        <div class="text-color-white" style="font-size:0.8em; margin-bottom:8px">Amateur&nbsp;<span class="text-color-3">1/11</span></div>';
-    content += '                                            <div style="padding:0 4px">';
-    content += '                                                <div class="bg-color-1" style="margin-bottom:4px; border-radius:7px; width:calc(100% - 4px); padding:2px">';
-    content += '                                                    <div class="bg-color-6" style="height:10px; width:60%; border-radius:5px"></div>';
-    content += '                                                </div>';
-    content += '                                            </div>';
-    content += '                                        </div>';
+    content += '                                        <div class="text-align-center day-label" style="font-size:1em; margin-bottom:8px">Day 1</div>';
     content += '                                    </div>';
     content += '                                </div>';
-    content += '                                <div class="text-align-center" style="margin-bottom:12px">';
-    content += '                                    <button type="button" class="button button-fill bg-color-5 rounded-8 text-color-white" style="font-size:1.2em; padding:22px 0; width:210px">Claim</button>';
+    content += '                            </div>';
+    content += '                            <div class="" style="padding:5px; width:25%">';
+    content += '                                <div class="card rounded-8 no-margin daily-reward-day active" style="height:80px">';
+    content += '                                    <div class="card-content card-content-padding rounded-8" style="padding:6px">';
+    content += '                                        <div class="text-align-center day-label" style="font-size:1em; margin-bottom:8px">Day 2</div>';
+    content += '                                    </div>';
     content += '                                </div>';
     content += '                            </div>';
+    content += '                            <div class="" style="padding:5px; width:25%">';
+    content += '                                <div class="card rounded-8 no-margin daily-reward-day" style="height:80px">';
+    content += '                                    <div class="card-content card-content-padding rounded-8" style="padding:6px">';
+    content += '                                        <div class="text-align-center day-label" style="font-size:1em; margin-bottom:8px">Day 3</div>';
+    content += '                                    </div>';
+    content += '                                </div>';
+    content += '                            </div>';
+    content += '                            <div class="" style="padding:5px; width:25%">';
+    content += '                                <div class="card rounded-8 no-margin daily-reward-day" style="height:80px">';
+    content += '                                    <div class="card-content card-content-padding rounded-8" style="padding:6px">';
+    content += '                                        <div class="text-align-center day-label" style="font-size:1em; margin-bottom:8px">Day 4</div>';
+    content += '                                    </div>';
+    content += '                                </div>';
+    content += '                            </div>';
+    content += '                        </div>';
+    content += '                       <div class="display-flex" style="width:100%">';
+    content += '                            <div class="" style="padding:5px; width:25%">';
+    content += '                                <div class="card rounded-8 no-margin daily-reward-day" style="height:80px">';
+    content += '                                    <div class="card-content card-content-padding rounded-8" style="padding:6px">';
+    content += '                                        <div class="text-align-center day-label" style="font-size:1em; margin-bottom:8px">Day 5</div>';
+    content += '                                    </div>';
+    content += '                                </div>';
+    content += '                            </div>';
+    content += '                            <div class="" style="padding:5px; width:25%">';
+    content += '                                <div class="card rounded-8 no-margin daily-reward-day" style="height:80px">';
+    content += '                                    <div class="card-content card-content-padding rounded-8" style="padding:6px">';
+    content += '                                        <div class="text-align-center day-label" style="font-size:1em; margin-bottom:8px">Day 6</div>';
+    content += '                                    </div>';
+    content += '                                </div>';
+    content += '                            </div>';
+    content += '                            <div class="" style="padding:5px; width:25%">';
+    content += '                                <div class="card rounded-8 no-margin daily-reward-day" style="height:80px">';
+    content += '                                    <div class="card-content card-content-padding rounded-8" style="padding:6px">';
+    content += '                                        <div class="text-align-center day-label" style="font-size:1em; margin-bottom:8px">Day 7</div>';
+    content += '                                    </div>';
+    content += '                                </div>';
+    content += '                            </div>';
+    content += '                            <div class="" style="padding:5px; width:25%">';
+    content += '                                <div class="card rounded-8 no-margin daily-reward-day" style="height:80px">';
+    content += '                                    <div class="card-content card-content-padding rounded-8" style="padding:6px">';
+    content += '                                        <div class="text-align-center day-label" style="font-size:1em; margin-bottom:8px">Day 8</div>';
+    content += '                                    </div>';
+    content += '                                </div>';
+    content += '                            </div>';
+    content += '                        </div>';
+    content += '                       <div class="display-flex" style="width:100%">';
+    content += '                            <div class="" style="padding:5px; width:25%">';
+    content += '                                <div class="card rounded-8 no-margin daily-reward-day" style="height:80px">';
+    content += '                                    <div class="card-content card-content-padding rounded-8" style="padding:6px">';
+    content += '                                        <div class="text-align-center day-label" style="font-size:1em; margin-bottom:8px">Day 9</div>';
+    content += '                                    </div>';
+    content += '                                </div>';
+    content += '                            </div>';
+    content += '                            <div class="" style="padding:5px; width:25%">';
+    content += '                                <div class="card rounded-8 no-margin daily-reward-day" style="height:80px">';
+    content += '                                    <div class="card-content card-content-padding rounded-8" style="padding:6px">';
+    content += '                                        <div class="text-align-center day-label" style="font-size:1em; margin-bottom:8px">Day 10</div>';
+    content += '                                    </div>';
+    content += '                                </div>';
+    content += '                            </div>';
+    content += '                            <div class="" style="padding:5px; width:25%; visibility:hidden">';
+    content += '                                <div class="card rounded-8 no-margin daily-reward-day" style="height:80px">';
+    content += '                                    <div class="card-content card-content-padding rounded-8" style="padding:6px">';
+    content += '                                        <div class="text-align-center day-label" style="font-size:1em; margin-bottom:8px">Day 11</div>';
+    content += '                                    </div>';
+    content += '                                </div>';
+    content += '                            </div>';
+    content += '                            <div class="" style="padding:5px; width:25%; visibility:hidden">';
+    content += '                                <div class="card rounded-8 no-margin daily-reward-day" style="height:80px">';
+    content += '                                    <div class="card-content card-content-padding rounded-8" style="padding:6px">';
+    content += '                                        <div class="text-align-center day-label" style="font-size:1em; margin-bottom:8px">Day 12</div>';
+    content += '                                    </div>';
+    content += '                                </div>';
+    content += '                            </div>';
+    content += '                        </div>';
+    content += '                        <div class="text-align-center" style="margin-top:30px; margin-bottom:18px">';
+    content += '                            <button type="button" class="button button-fill bg-color-5 rounded-8 text-color-white" style="font-size:1.2em; padding:22px 0; width:210px">Claim</button>';
     content += '                        </div>';
     content += '                    </div>';
     content += '               </div>';
